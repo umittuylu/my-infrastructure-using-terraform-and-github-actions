e30ec22fb8a374eb48632766f90b7cc87b529586
Ümit Tüylü <u.tuylu@gmail.com>
2026-07-27 21:31:49 +0300

    Format Terraform files

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,7 +20,7 @@ variable "ami_id" {
   description = "AMI ID (Amazon Linux 2023 önerilir)"
   type        = string
   # Örnek: eu-central-1 için Amazon Linux 2023
-  default     = "ami-0c55b159cbfafe1f0" # kendi region'ına göre güncelle
+  default = "ami-0c55b159cbfafe1f0" # kendi region'ına göre güncelle
 }
 
 variable "key_name" {
