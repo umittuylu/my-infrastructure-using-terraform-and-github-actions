7ceaaddae1c93972066ae180338fc269ed53e050
Ümit Tüylü <35814901+umittuylu@users.noreply.github.com>
2026-07-27 19:55:02 +0300

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,1 +1,19 @@
+terraform {
+  required_version = ">= 1.5.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  # State'i S3'te tutmak istersen (önerilir)
+  # backend "s3" {
+  #   bucket         = "senin-terraform-state-bucket"
+  #   key            = "ec2-s3/terraform.tfstate"
+  #   region         = "eu-central-1"
+  #   dynamodb_table = "terraform-locks"
+  #   encrypt        = true
+  # }
+}
