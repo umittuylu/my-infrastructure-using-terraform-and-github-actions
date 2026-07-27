f09ad01f22798d9032842d3fd6c7b4b3686151fe
Ümit Tüylü <35814901+umittuylu@users.noreply.github.com>
2026-07-27 22:37:44 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,23 +17,22 @@ variable "instance_type" {
 }
 
 variable "ami_id" {
-  description = "AMI ID (Amazon Linux 2023 önerilir)"
+  description = "AMI ID)"
   type        = string
-  # Örnek: eu-central-1 için Amazon Linux 2023
-<<<<<<< HEAD
-  default = "ami-0c55b159cbfafe1f0" # kendi region'ına göre güncelle
+ ----------------------------------------------------
+  default = "ami-0c55b159cbfafe1f0" 
 =======
   default     = "ami-0acfa5dc12e39973f" 
 >>>>>>> 908c77312396b4c9f522a7a012c83a19db74a8df
 }
 variable "key_name" {
-  description = "EC2 Key Pair adı (opsiyonel)"
+  description = "EC2 Key Pair name"
   type        = string
   default     = null
 }
 
 variable "allowed_ssh_cidr" {
-  description = "SSH için izin verilecek CIDR"
+  description = "CIDR blocks allowed for SSH access""
   type        = string
-  default     = "0.0.0.0/0" # Production'da kendi IP'nle sınırla!
+  default     = "0.0.0.0/0"
 }
