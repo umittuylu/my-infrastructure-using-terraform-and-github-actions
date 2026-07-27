59cddd2ca372cf2837af3e099fcfb887251b8a7d
Ümit Tüylü <35814901+umittuylu@users.noreply.github.com>
2026-07-27 20:00:01 +0300

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,15 @@
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
+}
 
+output "ec2_instance_id" {
+  value = aws_instance.web.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.ec2_sg.id
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.app_bucket.bucket
+}
